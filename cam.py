72458735cbee3f79f6412b661375f35dce876a01
dolpast <teroru720phi@gmail.com>
2023-06-29 01:55:14 +0900

    コードの見た目を整え、コメント文を追加しました

--- cam.py
+++ cam.py
@@ -6,26 +6,20 @@ ip_address = '127.0.0.1'
 port = 8081
 
 img = cv2.imread('nicecat.jpg')
-# print(type(img))
-# cv2.imshow('image', img)
-# while True:
-#     k = cv2.waitKey(1)
-#     if k == 13:
-#         break
 imgsiz = img.size
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 result, imgdata = cv2.imencode('.jpg', img, encode_param)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((ip_address, port))
-    sock.listen(1)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_cam:
+    sock_cam.bind((ip_address, port))
+    sock_cam.listen(1)
 
     while True:
         print('写真撮影要求信号を待機しています…')
-        sock2, addr = sock.accept()
-        with sock2:
+        sock_main, addr = sock_cam.accept()
+        with sock_main:
             print('接続しました：', addr)
-            recvbuf = sock2.recv(6)
+            recvbuf = sock_main.recv(6)
             if not recvbuf:
                 break
             prefix = struct.unpack('!6s', recvbuf)[0]
@@ -34,9 +28,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 break
             print('写真撮影要求信号を受信しました')
             sendbuf = struct.pack('!6si', b'IMGSIZ', imgsiz)
-            sock2.sendall(sendbuf)
+            sock_main.sendall(sendbuf)
             print('写真データサイズ予告信号を送信しました')
-            recvbuf = sock2.recv(6)
+            recvbuf = sock_main.recv(6)
             if not recvbuf:
                 break
             prefix = struct.unpack('!6s', recvbuf)[0]
@@ -44,6 +38,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             if prefix != b'OKNEXT':
                 break
             print('続行催促信号を受信しました')
-            sock2.sendall(b'IMGDAT')
-            sock2.sendall(imgdata)
+            sock_main.sendall(b'IMGDAT')
+            sock_main.sendall(imgdata)
             print('写真データ信号を送信しました')
--- sens.py
+++ sens.py
@@ -7,6 +7,6 @@ ip_addr = '127.0.0.1'
 port = 8080
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((ip_addr, port))           # サーバに接続を要求する
-    sock.sendall(b'SENSED')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_main:
+    sock_main.connect((ip_addr, port))  # 本体ユニット接続を要求する
+    sock_main.sendall(b'SENSED')
